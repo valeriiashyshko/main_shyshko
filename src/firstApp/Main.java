78ccd2200bef5e819d7e2367370769a4d8383323
Valeriia Shyshko <lera.sh95@gmail.com>
2023-09-23 12:50:47 +0200

    init commit

---
+++ src/firstApp/Main.java
@@ -0,0 +1,9 @@
+package firstApp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("I'm excited to learn Java");
+    }
+}
+
