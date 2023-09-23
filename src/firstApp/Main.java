c96397383730b050482b03cc9a2d003f230fa84f
Valeriia Shyshko <lera.sh95@gmail.com>
2023-09-23 15:57:32 +0200

    new branch commit

--- src/firstApp/Main.java
+++ src/firstApp/Main.java
@@ -3,7 +3,7 @@ package firstApp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("I'm excited to learn Java");
+        System.out.println("I'm excited to learn Java. This is a new branch");
     }
 }
 
